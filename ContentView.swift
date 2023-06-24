306e24ff4ce5b1f54b6655cb809a09c18149b8df
Alhoor Sultan <alhooralh@gmail.com>
2023-06-24 14:12:52 +0400

    change to Dubai

--- ContentView.swift
+++ ContentView.swift
@@ -6,7 +6,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
-            Text("Hello, Ajman")
+            Text("Hello, Dubai")
         }
     }
 }
